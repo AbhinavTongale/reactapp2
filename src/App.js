8a83902c1a130475b250d58f1d25e8b21d03d3cd
[Abhinavtongale] <abhitongale0123@gmail.com>
2022-12-20 14:16:03 +0530

    b2-jsx

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import "./App.css";
-
+// JSX!
+// JSX => JAVA SCRIPT + XML
+// XML => Extended the HTML and Cratedad new Tag
+// App => <App />
 function App() {
-  let mystyle = { color: "white", background: "purple" };
-
-  return (
-    <div>
-      <h1 className="beautify">Hello Again!</h1>
-
-      {/** This is kind of internal. Note: Only one curly braces. */}
-      <h1 style={mystyle}>Hello World</h1>
-
-      {/* THIS IS INLINE STYLING */}
-      <h1 style={{ color: "white", backgroundColor: "red" }}>Hello</h1>
-    </div>
-  );
+  return <h1>Hello</h1>;
 }
+
+/*function normalFn() {
+  // ..
+  // ..
+  // ..
+  return 100;
+  return "abcd"
+  return true;
+  return [];
+  return {};
+}*/
 
 export default App;
