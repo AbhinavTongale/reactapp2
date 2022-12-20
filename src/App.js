a175c40a4492bc9c4902ef709f624f70cb2168b0
[Abhinavtongale] <abhitongale0123@gmail.com>
2022-12-20 14:50:48 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-// Multi Tag
+import "./App.css";
+
 function App() {
+  let mystyle = { color: "white", background: "purple" };
+
   return (
     <div>
-      <h1>Div and H1</h1>
+      <h1 className="beautify">Hello Again!</h1>
+
+      {/** This is kind of internal. Note: Only one curly braces. */}
+      <h1 style={mystyle}>Hello World</h1>
+
+      {/* THIS IS INLINE STYLING */}
+      <h1 style={{ color: "white", backgroundColor: "red" }}>Hello</h1>
     </div>
   );
 }
